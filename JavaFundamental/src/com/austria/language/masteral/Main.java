25ac53bf47d287cc86290a160238993862eb96ec
vel02 <arielaustria021@gmail.com>
2020-04-22 15:23:08 +0800

    Another Commit

--- JavaFundamental/src/com/austria/language/masteral/Main.java
+++
@@ -1,8 +0,0 @@
-package com.austria.language.masteral;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ JavaFundamental/src/com/austria/language/masteral/ac_strings/scape/Main.java
@@ -0,0 +1,15 @@
+package com.austria.language.masteral.ac_strings.scape;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Scape Sequence
+
+        // c:\Windows\...
+        String message = "Hello, \"World!\"";
+        System.out.println(message);
+
+        String directory = "\nc:\t\\Windows\\...";
+        System.out.println(directory);
+    }
+}
